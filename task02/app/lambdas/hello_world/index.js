2085e2138607ce3a2f5a8b77dbbb5c2af51fe1e3
devmalya_saha <dp9458@srmist.edu.in>
2025-03-08 08:23:17 +0530

    Updated

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,21 +1,34 @@
 exports.handler = async (event) => {
-    console.log("Request event:", event);
+    console.log("Received event:", event); // For debugging purposes.
 
+    // Extract the path and HTTP method from the event object
     const { rawPath, httpMethod } = event;
 
+    // Case 1: Valid request to /hello with the GET method
     if (rawPath === "/hello" && httpMethod === "GET") {
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: "Hello, World!"
+                statusCode: 200,
+                message: "Hello from Lambda"
             }),
-        };
-    } else {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                error: `Bad Request: ${httpMethod} ${rawPath} is not supported.`,
-            }),
+            headers: {
+                "content-type": "application/json"
+            },
+            isBase64Encoded: false
         };
     }
+
+    // Case 2: Invalid request to any other endpoint or using an unsupported HTTP method
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
+            statusCode: 400,
+            message: `Bad request syntax or unsupported method. Request path: ${rawPath}. HTTP method: ${httpMethod}`
+        }),
+        headers: {
+            "content-type": "application/json"
+        },
+        isBase64Encoded: false
+    };
 };
