527ba3c4324b999847998783244bc6e99825be7d
devmalya_saha <dp9458@srmist.edu.in>
2025-03-08 08:45:32 +0530

    Updated

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,34 +1,35 @@
-exports.handler = async (event) => {
-    console.log("Received event:", event); // For debugging purposes.
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-    // Extract the path and HTTP method from the event object
-    const { rawPath, httpMethod } = event;
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    // Log event for debugging purposes
+    console.log("Event received:", JSON.stringify(event, null, 2));
 
-    // Case 1: Valid request to /hello with the GET method
+    const rawPath = event.path || event.rawPath || undefined;
+    const httpMethod = event.httpMethod || undefined;
+
     if (rawPath === "/hello" && httpMethod === "GET") {
         return {
             statusCode: 200,
             body: JSON.stringify({
                 statusCode: 200,
-                message: "Hello from Lambda"
+                message: "Hello from Lambda",
             }),
             headers: {
-                "content-type": "application/json"
+                "content-type": "application/json",
             },
-            isBase64Encoded: false
+            isBase64Encoded: false,
         };
     }
 
-    // Case 2: Invalid request to any other endpoint or using an unsupported HTTP method
     return {
         statusCode: 400,
         body: JSON.stringify({
             statusCode: 400,
-            message: `Bad request syntax or unsupported method. Request path: ${rawPath}. HTTP method: ${httpMethod}`
+            message: `Bad request syntax or unsupported method. Request path: ${rawPath}. HTTP method: ${httpMethod}`,
         }),
         headers: {
-            "content-type": "application/json"
+            "content-type": "application/json",
         },
-        isBase64Encoded: false
+        isBase64Encoded: false,
     };
 };
